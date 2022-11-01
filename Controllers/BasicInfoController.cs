315bb3833dd7b25ba83533ef327709c3e4cdeef5
rachnakafle <rachanakafle99@gmail.com>
2022-11-01 17:35:30 +0545

    Project Initialzed

---
+++ Controllers/BasicInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Candidate_Info.Controllers
+{
+    public class BasicInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/BasicInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Candidate_Info.Models
+{
+    public class BasicInfo
+    {
+        public int Id { get; set; }
+        public string Pradesh { get; set; }
+        public string District { get; set; }
+        public string PratinidhiSabha { get; set; }
+        public string PradeshSabha { get; set; }
+        public string RajnitikDal { get; set; }
+        public string Ummedwar { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
